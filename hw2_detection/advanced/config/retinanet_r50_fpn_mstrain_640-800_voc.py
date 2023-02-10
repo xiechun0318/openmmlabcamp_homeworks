2fdcb099e24fc28907ba363d0318d8fd41a3c050
xiechun0318 <xiechun0318@gmail.com>
2023-02-11 00:22:09 +0900

    add advanced

---
+++ hw2_detection/advanced/config/retinanet_r50_fpn_mstrain_640-800_voc.py
@@ -0,0 +1,22 @@
+_base_ = [
+    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/datasets/voc0712.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    bbox_head=dict(
+        num_classes=20))
+
+data = dict(
+    samples_per_gpu=8)
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+
+
+# learning policy
+# lr_config = dict(step=[16, 22])
+# runner = dict(type='EpochBasedRunner', max_epochs=24)
+
+load_from = 'checkpoints/retinanet/retinanet_r50_fpn_mstrain_3x_coco_20210718_220633-88476508.pth'
