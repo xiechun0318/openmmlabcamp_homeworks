66d03199a5c6ea82901ed177b5a8154977aafb62
xiechun0318 <xiechun0318@gmail.com>
2023-02-11 13:37:48 +0900

    update retinanet_r50_fpn_voc.py

--- hw2_detection/advanced/config/retinanet_r50_fpn_voc.py
+++ hw2_detection/advanced/config/retinanet_r50_fpn_voc.py
@@ -4,12 +4,14 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
+
 model = dict(
     bbox_head=dict(
         num_classes=20))
 
 data = dict(
     samples_per_gpu=8)
+evaluation=dict(save_best='auto')
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
@@ -17,6 +19,7 @@ optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 
 # learning policy
 # lr_config = dict(step=[16, 22])
-# runner = dict(type='EpochBasedRunner', max_epochs=24)
+runner = dict(type='EpochBasedRunner', max_epochs=8)
+log_config = dict(interval=200)
 
 load_from = 'checkpoints/retinanet/retinanet_r50_fpn_mstrain_3x_coco_20210718_220633-88476508.pth'
