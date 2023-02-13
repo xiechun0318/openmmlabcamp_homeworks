58cd31165c8ed1b58220dedff06d67dcb8a7407a
xiechun0318 <xiechun0318@gmail.com>
2023-02-14 00:30:02 +0900

    hw3

---
+++ hw3_segmentation/advanced/config/fast_scnn_voc12.py
@@ -0,0 +1,58 @@
+_base_ = [
+    './configs/_base_/models/fast_scnn.py', './configs/_base_/datasets/pascal_voc12.py',
+    './configs/_base_/default_runtime.py', './configs/_base_/schedules/schedule_20k.py'
+]
+
+norm_cfg = dict(type='BN', requires_grad=True, momentum=0.01)
+
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+model = dict(data_preprocessor=data_preprocessor, 
+             decode_head=dict(num_classes=21), 
+             auxiliary_head=[
+                 dict(type='FCNHead',
+                      in_channels=128,
+                      channels=32,
+                      num_convs=1,
+                      num_classes=21,
+                      in_index=-2,
+                      norm_cfg=norm_cfg,
+                      concat_input=False,
+                      align_corners=False,
+                      loss_decode=dict(type='CrossEntropyLoss', 
+                                       use_sigmoid=True, 
+                                       loss_weight=0.4)),
+                dict(
+                    type='FCNHead',
+                    in_channels=64,
+                    channels=32,
+                    num_convs=1,
+                    num_classes=21,
+                    in_index=-3,
+                    norm_cfg=norm_cfg,
+                    concat_input=False,
+                    align_corners=False,
+                    loss_decode=dict(
+                        type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.4)),])
+
+
+# Re-config the data sampler.
+train_dataloader = dict(batch_size=4, num_workers=4)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
+
+# Re-config the optimizer.
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=4e-5)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
+
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=2000)
+
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=200, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000, save_best='auto'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
+
+load_from = './fast_scnn_lr0.12_8x4_160k_cityscapes_20210630_164853-0cec9937.pth'
---
+++ hw3_segmentation/basic/README.md
@@ -0,0 +1,1 @@
+[model]("https://o365tsukuba-my.sharepoint.com/:u:/g/personal/xie_chun_gu_u_tsukuba_ac_jp/EfJLqCJBYqNPm0TPW8cPFEcByFNgCv_j8W6_Jk8QzM7lOA?e=HkG00G")
